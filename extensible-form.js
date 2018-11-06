43b9f91a2650de2befed156fab97189ff1ff0e85
Blake <ama542blake@gmail.com>
2018-11-06 00:15:25 -0500

    Trying to add options to multiple choice questions. So far no success.

--- extensible-form.js
+++ extensible-form.js
@@ -16,8 +16,17 @@ $("#tr").click(function () {
 
 // add a multiple response question
 $("#ms").click(function() {
-    $("#qBlock").append(questionTextHTML(questionCounter));
-    
+    $("#qBlock").append(questionTextHTML(questionCounter) + addOptionHTML(questionCounter));
+    // create callback for the button that was just created
+    $("#button" + questionCounter).click(function() {
+        console.log("in");
+        //var optionNum = 1;
+        //var questionNum = questionCounter;
+        $("#button" + questionCounter).parent().append(msHTML(6, 9));
+        //optionNum++;
+        console.log("out");
+    });
+    questionCounter++;
 });
 
 $("#ss").click(function() {
@@ -25,12 +34,27 @@ $("#ss").click(function() {
 });
 
 // creates HTML for adding question text
+// TODO: add name attribute once we figure out how the backend needs to receive data
 function questionTextHTML(counter) {
-    return '<div class="form-group input-field"><label for="question' + counter + '">Question ' + counter + '</label><input type="text" class="form-control" id="question' + counter + '" name="q' + counter + 'text" placeholder="Enter question prompt here"></div>'
+    return '<div class="form-group input-field" id="question"' + counter + '">'
+        + '<label>Question ' + counter
+        + '<input type="text" class="form-control">'
+        + '</label></div>';
 }
 
 // TODO: better define access to this data based on ID or name
 // generates the HTML for the text box that will take the user's response
 function textResponseHTML() {
     return '<textarea name="textArea" rows="5" cols="25"></textarea>'
+}
+
+function addOptionHTML(counter) {
+    return '<button type="button" class="btn btn-default" id="button' + counter + '">+</button>';
+}
+
+function msHTML(questionNum, optionNum) {
+    return '<label>Option ' + optionNum
+           + '<input type=text class="form-control" '
+           + 'id="question' + questionNum + '-option'+ optionNum
+           + '"></label>';
 }

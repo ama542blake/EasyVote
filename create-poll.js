af91811ce0db1e036e69629f84154902835b06f1
Blake <ama542blake@gmail.com>
2018-11-11 13:44:45 -0500

    Made changes to how questions and options are added and referenced

--- create-poll.js
+++ create-poll.js
@@ -11,8 +11,9 @@ var questions = new Array(1);
 /* -------------------- V Text Responses V -------------------- */
 $("#tr").click(function () {
    var newQuestion = new TextResponse();
-   //newQuestion.questionInputHTML;
-    console.log(newQuestion);
+    questions[questionCount - 1] = newQuestion;
+    console.log(questions[questionCount - 1]);
+   console.log(questions);
     newQuestion.questionInputHTML();
    questionCount++;
 });
@@ -34,13 +35,13 @@ function TextResponse() {
 
 $("#ss").click(function () {
    var newQuestion = new SingleSelection();
-   //newQuestion.questionInputHTML;
-    console.log(newQuestion);
+    questions[questionCount - 1] = newQuestion;
+    console.log(questions[questionCount - 1]);
+   console.log(questions);
+    //newQuestion.boundQuestionInputHTML();
     newQuestion.questionInputHTML();
    questionCount++;
 });
-
-
 
 // constructs a Single Selection multiple choice question
 function SingleSelection() {
@@ -48,34 +49,37 @@ function SingleSelection() {
     this.optionCount = 3;
     this.question = this;
     this.questionInputHTML = function () {
+        console.log(this);
         if (questionCount > MAX_QUESTIONS) {
             alert("Sorry, the maximum number of questions allowed is " + MAX_QUESTIONS + ".");
         } else {
+            // the .append method changes scope, so to keep a reference to the current question, so thisQuestion keeps a reference to it
+            var thisQuestion = this;
             $("#qBlock").append('<div class="form-group input-field" id="SS_q' + this.questionNumber + '">'
                                     + '<label>Question ' + this.questionNumber
                                         + '<input type="text" class="form-control" name="SS_q"' + '>'
                                     + '</label>'
-                                    + '<label>Option 1'
-                                        + '<input type="text" class="form-control" id="SS_q' + this.questionNumber + '_o1" name="SS_q' + this.questionNumber + '_o1">'
-                                    + '</label>'
-                                    + '<label>Option 2'
-                                        + '<input type="text" class="form-control" id="SS_q' + this.questionNumber + '_o2" name="SS_q' + this.questionNumber + '_o2">'
-                                    + '</label>'
+                                        +'<div id="SS_q' + this.questionNumber + '_options">'
+                                            + '<label class="option">Option 1'
+                                                + '<input type="text" class="form-control" id="SS_q' + this.questionNumber + '_o1" name="SS_q' +    this.questionNumber + '_o1">'
+                                            + '</label>'
+                                            + '<label class="option">Option 2'
+                                                + '<input type="text" class="form-control" id="SS_q' + this.questionNumber + '_o2" name="SS_q' +    this.questionNumber + '_o2">'
+                                            + '</label>'
+                                        + '</div>'
                                     + '<button type="button" class="btn btn-default" id="btnAdd' + this.questionNumber + '">Add Option</button>'
                               + '</div>');
                 $("#btnAdd" + this.questionNumber).click(function () {
-                    console.log(this);
-                    if (this.optionCount > MAX_OPTIONS) {
+                    if (thisQuestion.optionCount > MAX_OPTIONS) {
                         alert("Sorry, you may not add more than " + MAX_OPTIONS + " options to a single selection question.");
                     } else {
-                        $("#qBlock").append(
-                             '<label>Option ' + this.optionCount
-                                + '<input type="text" class="form-control" id="SS_q' + this.questionCount + '_o' + this.optionCount + '"name="SS_q' + this.questionCount + '_o' + this.optionCount + '">'
+                        $("#SS_q" + thisQuestion.questionNumber + '_options').append(
+                             '<label class="option">Option ' + thisQuestion.optionCount
+                                + '<input type="text" class="form-control" id="SS_q' + thisQuestion.questionNumber + '_o' + thisQuestion.optionCount + '"name="SS_q' + thisQuestion.questionNumber + '_o' + thisQuestion.optionCount + '">'
                             + '</label>');
-                        alert("click");
+                        thisQuestion.optionCount++;
                     }
                 });
-            this.optionCount++;
         };
     };
 }
@@ -87,12 +91,12 @@ function SingleSelection() {
 //        this.qNum = qn;
 //        this.qText = qt;
 //        // functions the same way as questionCount
-//        // starts at 3 because 1 and 2 are constucted with the 
+//        // starts at 3 because 1 and 2 are constucted with the
 //        this.optionCount = 3;
 //        // there should be at least 2 options in every SS question
 //        this.options = new Array(2);
 //    },
-//    
+//
 //    questionInputHTML: function (questionNumber) {
 //        $("#qBlock").append('<div class="form-group input-field" id="SS_q' + questionNumber + '">'
 //                    + '<label>Question ' + questionNumber
@@ -102,7 +106,7 @@ function SingleSelection() {
 //                    + SingleSelection.optionHTML(questionNumber, 1) + SingleSelection.optionHTML(questionNumber, 2)
 //              + '</div>');
 //    },
-//    
+//
 //    optionHTML: function (questionNumber, optionNumber) {
 //        return '<label>'
 //                    + '<input type="text" class="form-control" id="SS_q' + questionNumber + '_o' + optionNumber + '" name="SS_q' + questionNumber + '_o' + optionNumber + '">'

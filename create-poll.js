4a83706385d0e4cb6852929eaef9d9a98966c05f
Blake <ama542blake@gmail.com>
2018-11-12 00:35:35 -0500

    Rewrote eveything AGAIN

--- create-poll.js
+++ create-poll.js
@@ -7,104 +7,124 @@ var questionCount = 1;
 // array that holds all questions
 var questions = new Array(1);
 
-/* Functions for adding questions to the form */
-
-$("#tr").click(function () {
-   var newQuestion = new TextResponse();
-    questions[questionCount - 1] = newQuestion;
-    console.log(questions[questionCount - 1]);
-    console.log(questions);
-    newQuestion.questionInputHTML();
-   questionCount++;
-});
-
-function TextResponse() {
-    this.questionNumber = questionCount;
-    this.questionInputHTML = function () {
-        if (questionCount > MAX_QUESTIONS) {
-            alert("Sorry, the maximum number of questions allowed is " + MAX_QUESTIONS + ".");
-        } else {
-            $("#qBlock").append('<div class="form-group input-field" id="TR_q' + questionCount + '">'
-                                + '<label>Question ' + questionCount
-                                    + '<input type="text" class="form-control" name="TR_q"' + '>'
-                                + '</label>'
-                              + '</div>');
-            }
-    };
-};
-
-
-/* Creates a single selection question, then adds it to the array of questions and displays it on the page */
-$("#ss").click(function () {
-   var newQuestion = new MultipleChoice("SS");
-    questions[questionCount - 1] = newQuestion;
-    newQuestion.questionInputHTML();
-   questionCount++;
-});
-
-/* Creates a multiplce selection question, then adds it to the array of questions and displays it on the page */
-$("#ms").click(function () {
-    var newQuestion = new MultipleChoice("MS");
-    questions[questionCount - 1] = newQuestion;
-    newQuestion.questionInputHTML();
+$("#tr").click(function() {
+    var newQuestion = new TextResponse();
+    $("#qBlock").append(newQuestion.qTextHTML());
+    questions[questionCount - 1] = newQuestion.id;
     questionCount++;
 });
 
-
-// constructs a Multiple Choice question (either multiple or single selection)
-//qType: string that represent which type of multiple choice question shold be - SS = single selection - MS = multiple selection
-function MultipleChoice(qType) {
-    this.questionNumber = questionCount;
-    this.optionCount = 3;
-    this.questionType = qType;
-    this.options = new Array(1);
-    // the .append method changes scope, so to keep a reference to the current question, so thisQuestion keeps a reference to it
-    thisQuestion = this;
-    this.questionInputHTML = function () {
-        if (questionCount > MAX_QUESTIONS) {
-            alert("Sorry, the maximum number of questions allowed is " + MAX_QUESTIONS + ".");
-        } else {
-            // the .append method changes scope, so to keep a reference to the current question, so thisQuestion keeps a reference to it
-            $("#qBlock").append('<div class="form-group input-field" id=' + thisQuestion.questionType + '_q' + this.questionNumber + '">'
-                                    + '<label>Question ' + this.questionNumber
-                                        + '<input type="text" class="form-control" name="' + thisQuestion.questionType +  '_q"' + '>'
-                                    + '</label>'
-                                        +'<div id="' + thisQuestion.questionType + '_q' + this.questionNumber + '_options">'
-                                            + '<label class="option">Option 1'
-                                                + '<input type="text" class="form-control" id="' + thisQuestion.questionType + '_q' + this.questionNumber + '_o1" name="_q' +    this.questionNumber + '_o1">'
-                                            + '</label>'
-                                            + '<label class="option">Option 2'
-                                                + '<input type="text" class="form-control" id="' + thisQuestion.questionType + '_q' + this.questionNumber + '_o2" name="' + thisQuestion.questionType + '_q' +    this.questionNumber + '_o2">'
-                                            + '</label>'
-                                        + '</div>'
-                                    + '<button type="button" class="btn btn-default" id="btnAdd' + this.questionNumber + '">Add Option</button>'
-                              + '</div>');
-                $("#btnAdd" + this.questionNumber).click(function () {
-                    if (thisQuestion.optionCount > MAX_OPTIONS) {
-                        alert("Sorry, you may not add more than " + MAX_OPTIONS + " options to a single selection question.");
-                    } else {
-                        $("#" + thisQuestion.questionType + "_q" + thisQuestion.questionNumber + '_options').append(Option(thisQuestion.questionNumber, thisQuestion.optionCount, thisQuestion.questionType));
-                        thisQuestion.optionCount++;
-                    }
-                });
-        };
+function TextResponse() {
+    this.qNum = questionCount;
+    this.id = 'TR_q' + this.qNum; 
+    
+    this.qTextHTML = function () {
+        return '<div class="form-group" id="' + this.id + '">'
+                    + '<label>Question ' + this.qNum 
+                        + '<input type="text" class="form-control">'
+                    +'</label>'
+             + '</div>';
     };
 }
 
-// construct the option text inputs
-function Option(questionNumber, optionNumber, questionType) {
-    return '<label class="option">Option ' + optionNumber
-                + '<input type="text" class="form-control" id="' + questionType + '_q' + questionNumber + '_o' + optionNumber + '"name="' + questionType + '_q' + questionNumber + '_o' + optionNumber + '">'
+
+$("#ss").click(function() {
+    var newQuestion = new MultipleChoice("SS");
+    $("#qBlock").append(newQuestion.qTextHTML());
+    questions[questionCount - 1] = newQuestion.id;
+    $('#' + newQuestion.id + '_options').append(addBtn(newQuestion.qNum)); 
+    $("#addBtn" + newQuestion.qNum).click(function () {
+        if (newQuestion.optionCount > MAX_OPTIONS) {
+            alert("Sorry, you may only add " + MAX_OPTIONS + " options to a multiple choice question.");
+        } else {
+            $('#' + newQuestion.id + '_options').append(addOption(newQuestion.id, newQuestion.optionCount));
+            newQuestion.optionCount++;
+        }
+    });
+    questionCount++;
+});
+
+$("#ms").click(function() {
+    var newQuestion = new MultipleChoice("MS");
+    $("#qBlock").append(newQuestion.qTextHTML());
+    questions[questionCount - 1] = newQuestion.id;
+    $('#' + newQuestion.id + '_options').append(addBtn(newQuestion.qNum)); 
+    $("#addBtn" + newQuestion.qNum).click(function () {
+        if (newQuestion.optionCount > MAX_OPTIONS) {
+            alert("Sorry, you may only add " + MAX_OPTIONS + " options to a multiple choice question.");
+        } else {
+            $('#' + newQuestion.id + '_options').append(addOption(newQuestion.id, newQuestion.optionCount));
+            newQuestion.optionCount++;
+        }
+    });
+                                          
+    questionCount++;
+});
+
+function MultipleChoice(qType) {
+    this.qNum = questionCount;
+    this.qType = qType;
+    this.optionCount = 3;
+    // this will hold the ID's of each option
+    this.options = new Array(1);
+    this.id = qType + "_q" + this.qNum;
+    
+    this.qTextHTML = function() {
+        return '<div class="form-group" id="' + this.id + '">'
+                    + '<label> Question ' + this.qNum 
+                        + '<input type="text" class="form-control">'
+                        + '<div class="form-group input-field" id="' + this.id + '_options">'
+                            + '<label class="option">Option 1'
+                                + '<input type="text" class="form-control" id="' + this.id + '_o1">'
+                            + '</label>'
+                            + '<label class="option">Option 2'
+                                + '<input type="text" class="form-control" id="' + this.id + '_o2">'
+                            + '</label>'
+                        + '</div'
+                    +'</label>'
+             + '</div>';
+    };
+    
+}
+
+function addBtn(qNum) {
+    return '<button type="button" class="btn btn-default" id="addBtn' + qNum + '">Add Option</button>';
+}
+
+function addOption(id, optNum) {
+    return '<label class="option">Option ' + optNum
+            + '<input type="text" class="form-control" id="' + id + '_o' + optNum + '">'
          + '</label>'
 }
 
-/* End of functions for adding questions to the form */
+$('input[name="theme"]').click(function () {
+  console.log("ok");
+  themeSelector();
+});
 
-
-
-
-/* Functions for removing questions from the form */
-
-
-
-/* End of unctions for removing questions from the form */
+function themeSelector() {
+    var selectedTheme = $('input[name=theme]:checked').val();
+    var body = document.querySelector("body");
+    switch (selectedTheme) {
+        case "Light":
+            body.style.backgroundColor = "#dadada";
+            body.style.color = "black";
+            break;
+            
+        case "Dark":
+            body.style.backgroundColor = "#232323";
+            body.style.color = "#777777";
+            break;
+        case "Forest":
+            body.style.backgroundColor = "#498645";
+            body.style.color = "#222222";
+            break;
+        case "Seaside":
+            body.style.backgroundColor = "#2A82ED";
+            body.style.color = "#222222";
+            break;
+        default:
+            body.style.backgroundColor = "#46C6C6";
+            body.style.color = "white;"
+    }
+}
